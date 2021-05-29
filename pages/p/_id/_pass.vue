3d9ece1a727104c34154f1eeaf457f975bbff0ad
Taiki Yamada <yamada@microweb.jp>
2021-05-30 02:19:30 +0900

    tukutta

---
+++ pages/p/_id/_pass.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <h1>{{ title }}</h1>
+    <p v-html="message">no message.</p>
+    <hr />
+    <div class="right">
+      <router-link to="/">Go to Top</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return !(params.id === undefined || params.pass === undefined)
+  },
+  data() {
+    return {
+      title: 'Login',
+    }
+  },
+  computed: {
+    message() {
+      const id = this.$route.params.id
+      const pass = this.$route.params.pass
+      return 'ID: ' + id + '<br>PASS: ' + pass
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+
+h1 {
+  font-size: 60pt;
+  color: #345980;
+}
+
+p {
+  padding-top: 5px;
+  font-size: 20px;
+}
+
+a {
+  font-size: 16pt;
+}
+
+.right {
+  text-align: right;
+}
+</style>
